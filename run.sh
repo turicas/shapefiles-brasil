58e094879771b4c8c1cd5c253ca8030d86110748
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2025-12-25 11:43:56 -0300

    Cria atalho para a tolerância padrão de cada nível

--- run.sh
+++ run.sh
@@ -25,6 +25,7 @@ function extract_shp() {
 function download_extract_upload() {
   state=$1; shift
   url=$1; shift
+  defaultTolerance=$1
 
   zip_filename="$DOWNLOAD_PATH/${state}.zip"
   wget -O "$zip_filename" -c -t 0 "$url"
@@ -33,7 +34,10 @@ function download_extract_upload() {
     mkdir -p $OUTPUT_PATH/$tolerance
     output="$OUTPUT_PATH/$tolerance/${state}.geojson"
     extract_shp "$zip_filename" "$tolerance" "$output"
-    s3cmd put "$output" s3://dataset/$DATASET/$tolerance/${state}.geojson
+    s3cmd put "$output" "s3://dataset/$DATASET/$tolerance/${state}.geojson"
+    if [[ $tolerance = $defaultTolerance ]]; then
+      s3cmd put "$output" "s3://dataset/$DATASET/${state}.geojson"
+    fi
     if [ "$tolerance" = "full" ]; then
       output_path="$OUTPUT_PATH/$tolerance/${state}"
       time python extrai_subfetuare.py "$output" "$output_path"
@@ -46,17 +50,17 @@ function download_extract_upload() {
   rm -rf "$zip_filename"
 }
 
+state="BR-UF"
+url="http://geoftp.ibge.gov.br/organizacao_do_territorio/malhas_territoriais/malhas_municipais/municipio_2018/Brasil/BR/br_unidades_da_federacao.zip"
+download_extract_upload "$state" "$url" "0.1"
+
 for state in $STATES; do
   echo $state
   state_lower=$(echo $state | tr A-Z a-z)
   url="http://geoftp.ibge.gov.br/organizacao_do_territorio/malhas_territoriais/malhas_municipais/municipio_2018/UFs/${state}/${state_lower}_municipios.zip"
-  download_extract_upload "$state" "$url"
+  download_extract_upload "$state" "$url" "0.005"
 done
 
 state="BR-municipios"
 url="http://geoftp.ibge.gov.br/organizacao_do_territorio/malhas_territoriais/malhas_municipais/municipio_2018/Brasil/BR/br_municipios.zip"
-download_extract_upload "$state" "$url"
-
-state="BR-UF"
-url="http://geoftp.ibge.gov.br/organizacao_do_territorio/malhas_territoriais/malhas_municipais/municipio_2018/Brasil/BR/br_unidades_da_federacao.zip"
-download_extract_upload "$state" "$url"
+download_extract_upload "$state" "$url" "0.01"
